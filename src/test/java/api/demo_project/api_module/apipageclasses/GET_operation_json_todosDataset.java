64f5556325c7adffa7c430c0a11dbf84d44c33b3
root <santhosh.annam@devrabbit.com>
2019-12-03 12:43:25 +0530

    test

---
+++ src/test/java/api/demo_project/api_module/apipageclasses/GET_operation_json_todosDataset.java
@@ -0,0 +1,67 @@
+package api.demo_project.api_module.apipageclasses; 
+
+public class GET_operation_json_todosDataset{
+
+public static final int datasetLength = 1; 
+
+public static String projectName = "demo_project";
+
+ public static String primaryInfo  = "{\"module_description\":\"desc\","
+ + "\"docker_files_directory_url\":\"https://smartqe.io:443/pdownload/DockerFiles\","
+ + "\"mobile_platform\":null,"
+ + "\"report_upload_url\":\"https://smartqe.io:443/UploadReportFile\","
+ + "\"repository_commit_message\":\"test\","
+ + "\"browser_type\":null,"
+ + "\"client_timezone_id\":\"Asia/Calcutta\","
+ + "\"docker_files_directory\":\"/opt/tomcat9/webapps/pdownload/DockerFiles\","
+ + "\"project_url\":\"https://jsonplaceholder.typicode.com/\","
+ + "\"jenkins_username\":\"devrabbit\","
+ + "\"testcase_id\":309,"
+ + "\"project_name\":\"Demo_Project\","
+ + "\"testset_name\":null,"
+ + "\"is_web\":false,"
+ + "\"jenkins_password\":\"rabbit\","
+ + "\"testcase_overwrite\":true,"
+ + "\"docker_file_name\":\"DockerFile_85_SE_TC_309\","
+ + "\"project_id\":454,"
+ + "\"framework_path\":\"/home/devrabbit/SmartQE/SmartQE-Users/85/Selenium/Demo_Project\","
+ + "\"sub_module_id\":0,"
+ + "\"repository_url\":\"nF2SrtwzI72sOpJsbXS0NSIn7+7UE2XiLXjunTrs/IQabmo7PU2UaTOdNu02vSIvA1xC1klvgxuYRi2lz2aXs3O+hbBvLbqUBCeNnsbEgV5fTHT0pgtK/lPIw0BCvnDT\","
+ + "\"sub_module_description\":null,"
+ + "\"testcase_name\":\"TC_GET_Operation_json_placeholders_API\","
+ + "\"executed_timestamp\":1575357202206,"
+ + "\"is_execute\":false,"
+ + "\"file_name\":null,"
+ + "\"aes_secret_key\":\"_SmartQE_SmartQE\","
+ + "\"testset_id\":0,"
+ + "\"jenkins_token\":\"114de69b453efd3524fce8756b1ed0e69c\","
+ + "\"is_generate\":false,"
+ + "\"jenkins_url\":\"http://192.168.1.141:8080/\","
+ + "\"project_description\":\"Desc\","
+ + "\"module_id\":776,"
+ + "\"user_id\":85,"
+ + "\"bundle_id\":null,"
+ + "\"sub_module_name\":null,"
+ + "\"device_os_version\":null,"
+ + "\"execution_environment\":\"jenkins\","
+ + "\"module_name\":\"API_Module\","
+ + "\"executed_user_id\":85,"
+ + "\"desktop_automation\":false,"
+ + "\"input_command\":null}";
+
+
+			/*--------Default Dataset-----------*/
+
+public static final String datasetHeader1 = "TC_GET_Operation_json_placeholders_API-Default";
+
+public static final String datasetResources1 = "todos";
+public static final int requestType1 = 1;
+public static final int bodyType1 = -1;
+
+ public static String urlParams1  = "[]";
+
+ public static String headers1  = "[]";
+
+ public static String body1  = "{}";
+
+}
